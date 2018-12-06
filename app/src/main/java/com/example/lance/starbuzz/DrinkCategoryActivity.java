78fa2c0864fa538d609f346e9444e7ee2427450e
SirLancelotZ <ngtlancezhu@gmail.com>
2018-12-06 00:17:13 -0800

    Starbuzz

--- app/src/main/java/com/example/lance/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/lance/starbuzz/DrinkCategoryActivity.java
@@ -1,29 +1,31 @@
 package com.example.lance.starbuzz;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class DrinkCategoryActivity extends AppCompatActivity {
-
+public class DrinkCategoryActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drink_category);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    super.onCreate(savedInstanceState);
+    ListView listDrinks = getListView();
+    ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>(this, android.R.layout.simple_list_item_1, Drink.drinks);
+    listDrinks.setAdapter(listAdapter);
+}
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    @Override
+    public void onListItemClick(ListView listView, View itemView, int position, long id) {
+        Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+        intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+        startActivity(intent);
     }
+
 
 }
